32bc7fabc9ed1ba063b5737dae9bedafd1862466
armphanu <armzazgakkub@gmail.com>
2021-04-23 22:19:24 +0700

    update route store

--- backend/routes/store.js
+++ backend/routes/store.js
@@ -23,7 +23,7 @@ router.put("/addstore", async function(req, res, next){
   await conn.beginTransaction();
   try {
     await conn.query("INSERT INTO store (subscription_type, store_name, description, seller_id, owner_marketplace_id) VALUES(?, ?, ?, ?, ?);", 
-    [rent_type, storename,description, req.session.userdata.id, 1])
+    [rent_type, storename,description, 2, 1])
     await conn.commit()
     res.json("addstorecomplete")
   } catch (err) {
@@ -85,6 +85,89 @@ router.put("/addproduct", upload.array('Pic'), async function(req, res, next){
     conn.release();
   }
 
+})
+
+router.get("/getproduct", async function(req, res, next){
+  const conn = await pool.getConnection();
+  await conn.beginTransaction();
+  try{
+    const [rows, _] = await pool.query("SELECT * FROM store where seller_id = ? ", [req.session.userdata.id])
+    const product = await pool.query("select * from product where store_id = ?", [rows[0].id])
+    res.send(product[0])
+    await conn.commit()
+  }catch (err) {
+    await conn.rollback();
+    return res.status(400).json(err);
+  } finally {
+    console.log("finally");
+    conn.release();
+  }
+})
+
+router.delete("/deleteproduct/:productId", async function(req,res,next){
+  const conn = await pool.getConnection();
+  await conn.beginTransaction();
+  try{
+    const [images,imageFields] = await conn.query(
+      "SELECT `picture` FROM `product_picture` WHERE `product_id` = ?",
+      [req.params.productId]
+    );
+    const appDir = path.dirname(require.main.filename); // Get app root directory
+      
+      images.forEach((e) => {
+        const p = path.join(appDir, 'static', e.picture);
+        fs.unlinkSync(p);
+      });
+      await conn.query("DELETE FROM `product_picture` WHERE `product_id` = ?", [req.params.productId]);
+      await conn.query("DELETE FROM `product` WHERE `id` = ?", [req.params.productId]);
+    await conn.commit()
+    res.status(204).send();
+  }catch (err) {
+    console.log(err)
+    await conn.rollback();
+    return res.status(500).json(err);
+  } finally {
+    conn.release();
+  }
+})
+
+router.get("/getproduct/:productId", async function(req, res, next){
+  const conn = await pool.getConnection();
+  await conn.beginTransaction();
+  try{
+    const product = await pool.query("SELECT * FROM product WHERE id = ?", [req.params.productId])
+    await conn.commit()
+    res.send(product[0][0])
+  }catch (err) {
+    console.log(err)
+    await conn.rollback();
+    return res.status(500).json(err);
+  } finally {
+    conn.release();
+  }
+})
+
+router.post("/editproduct/:productId", async function (req, res, next) {
+  const conn = await pool.getConnection();
+  await conn.beginTransaction();
+  const p_name = req.body.p_name
+  const price = req.body.price
+  const description = req.body.description
+  const quantity = req.body.quantity
+  const promotion_price = req.body.promotion_price
+  const categorys = req.body.category
+  try{
+    const category = await conn.query("SELECT * FROM category WHERE category_name = ?", [categorys])
+    await conn.query('UPDATE product SET p_name=?, price=?, description=?, promotion_price=?, quantity=?, category_id=? WHERE id=?', 
+    [p_name, price, description, promotion_price, quantity, category[0][0].id, req.params.productId])
+    res.send('update complete')
+  }catch (err) {
+    console.log(err)
+    await conn.rollback();
+    return res.status(500).json(err);
+  } finally {
+    conn.release();
+  }
 })
 
 
