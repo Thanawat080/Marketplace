c5e93d334e664cbef8b61000ed9287bcb7545600
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2021-04-26 21:54:29 +0700

    Add files via upload

---
+++ backend/routes/admin.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const pool = require("../config");
+const fs = require("fs");
+router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+exports.router = router;
