b46090b57d6fc2970af54c9b97c0f069cc0aee77
armphanu <armzazgakkub@gmail.com>
2021-04-27 16:12:00 +0700

    addhistory page

---
+++ myfrontend/src/views/orderHistory.vue
@@ -0,0 +1,45 @@
+<template>
+     <div class="container is-max-desktop">
+            <br><br>
+            <div class="columns">
+                <div class="column is-one-third">
+                    <aside class="menu">
+                        <p class="menu-label">
+                            หน้าหลัก
+                        </p>
+                        <ul class="menu-list">
+                            <li><a href="profile.html">หน้าหลัก</a></li>
+                            <li><a href="changepass.html">แก้ไขรหัส</a></li>
+                        </ul>
+                        <p class="menu-label">
+                            ประวัติ
+                        </p>
+                        <ul class="menu-list">
+                            <li><a href="history.html">ประวัติการซื้อ</a></li>
+                        </ul>
+                    </aside>
+                </div>
+                <div class="column is-two-thirds notification is-primary is-light">
+                    <div class="message-header">
+                        <p>ประวัติการซื้อ</p>
+                    </div>
+                    <br>
+                    <div class="column is-full">
+
+                        <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+                            style="width: 100%;">
+                            <!-- Your table content -->
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <td style="width: 50%;">Test</td>
+                                    <td>Track Order : xxxx</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+     </div>
+</template>
