beca1ad88735d108733edc5a61b10e3c12feeb72
Pommy <pomboivo@hotmail.com>
2021-04-30 22:08:18 +0700

    App.vue
    Update Design

--- myfrontend/src/App.vue
+++ myfrontend/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-
     <div class="container">
       <nav class="navbar is-transparent">
         <div class="navbar-brand">
-          <a class="navbar-item" style="color: black;">
+          <a class="navbar-item" style="color: black">
             <i class="fas fa-2x fa-shopping-cart"></i><b>MARKET</b>
           </a>
           <div
@@ -33,22 +32,18 @@
               >Register</router-link
             >
             <router-link class="card-footer-item navbar-item" to="/checkout"
-              ><i class="fas fa-shopping-cart"></i>&nbsp;{{
-                count
-              }}</router-link
+              ><i class="fas fa-shopping-cart"></i>{{'\xa0'}}{{ count }}</router-link
             >
             <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link"> <i class="fas fa-user"></i>{{'\xa0'}}{{ name }} </a>
 
-              <router-link class="navbar-link" to="/profile"
-              ><i class="fas fa-user"></i>&nbsp;</router-link
-            >
-              <div class="navbar-dropdown is-boxed">
-                <a
-                  class="navbar-item"
-                  href="https://bulma.io/documentation/overview/start/"
+              <div class="navbar-dropdown">
+                <router-link class="navbar-link" to="/profile"
+                  >Profile</router-link
                 >
-                  help
-                </a>
+                <a class="navbar-item"> Help </a>
+                <hr class="navbar-divider" />
+                <a class="navbar-item"> Logout </a>
               </div>
             </div>
           </div>
