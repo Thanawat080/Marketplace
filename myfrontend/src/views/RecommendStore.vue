028aac378974a708d5866930fb104cacb4c65b4c
Pommy <pomboivo@hotmail.com>
2021-05-02 16:03:20 +0700

    RecommendStore.vue

---
+++ myfrontend/src/views/RecommendStore.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="container is-max-desktop">
+      <div class="is-multiline columns is-variable is-2">
+        <br>
+        <!-- Start Recommend -->
+        <div class="column is-one-quarter">
+          <div class="card">
+            <div class="message-header">
+    <p>Dark</p>
+  </div>
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="content">
+      asdasd
+    </div>
+  </div>
+</div>
+        </div>
+        <!-- End Recommend -->
+      </div>
+    </div>
+
+    <br />
+  </div>
+</template>
