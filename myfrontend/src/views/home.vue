f3871429cca522d285fe2f2b53c39b9dd8016e3f
Pommy <pomboivo@hotmail.com>
2021-05-02 21:50:38 +0700

    home.vue

--- myfrontend/src/views/home.vue
+++ myfrontend/src/views/home.vue
@@ -112,9 +112,7 @@ import $ from "jquery";
 $("#slideshow > div:gt(0)").hide();
 setInterval(function () {
   $("#slideshow > div:first")
-    .fadeOut(1000)
     .next()
-    .fadeIn(1000)
     .end()
     .appendTo("#slideshow");
 }, 5000);
