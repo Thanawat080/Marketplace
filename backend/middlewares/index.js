b2d844d8708d0a275c03c128e9a4603992df3522
armphanu <armzazgakkub@gmail.com>
2021-05-04 15:18:16 +0700

    t

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -10,21 +10,7 @@ async function isLoggedIn (req, res, next) {
     if (!authorization) {
         return res.status(401).send('You are not logged in')
     }
-    
-    // // Check token
-    // const [tokens] = await pool.query('SELECT * FROM tokens WHERE token = ?', [part2])
-    // const token = tokens[0]
-    // if (!token) {
-    //     return res.status(401).send('You are not logged in')
-    // }
-
-    // Set user
-    // const [users] = await pool.query(
-    //     'SELECT id, username, first_name, last_name, email, picture, mobile, join_date ' + 
-    //     'FROM users WHERE id = ?', [token.user_id]
-    // )
     req.user = req.session.userdata
-
     next()
 }
 
