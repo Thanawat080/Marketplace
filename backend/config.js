a243fa92a11fe8e67e9086f9b0f4777ebd3fa219
armphanu <56469017+armphanu@users.noreply.github.com>
2021-05-04 19:10:03 +0700

    Add files via upload

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'jamew000',
+  password: 'arm_18273',
   database: 'project',
   waitForConnections: true,
   connectionLimit: 10,
